51fcbfcd38c7bb651701714cc517c2178c8475bb
Vladimir.Tyrin <b0-0b@yandex.ru>
2017-01-13 18:43:01 +0300

    NetworkUI added

---
+++ NetworkView.NetworkUI/NetworkView_NodeDragging.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace NetworkView.NetworkUI
+{
+    /// <summary>
+    /// Partial definition of the NetworkView class.
+    /// This file only contains private members related to dragging nodes.
+    /// </summary>
+    public partial class NetworkView
+    {
+        #region Private Methods
+
+        /// <summary>
+        /// Event raised when the user starts to drag a node.
+        /// </summary>
+        private void NodeItem_DragStarted(object source, NodeDragStartedEventArgs e)
+        {
+            e.Handled = true;
+
+            this.IsDragging = true;
+            this.IsNotDragging = false;
+            this.IsDraggingNode = true;
+            this.IsNotDraggingNode = false;
+
+            var eventArgs = new NodeDragStartedEventArgs(NodeDragStartedEvent, this, this.SelectedNodes);            
+            RaiseEvent(eventArgs);
+
+            e.Cancel = eventArgs.Cancel;
+        }
+
+        /// <summary>
+        /// Event raised while the user is dragging a node.
+        /// </summary>
+        private void NodeItem_Dragging(object source, NodeDraggingEventArgs e)
+        {
+            e.Handled = true;
+
+            //
+            // Cache the NodeItem for each selected node whilst dragging is in progress.
+            //
+            if (this.cachedSelectedNodeItems == null)
+            {
+                this.cachedSelectedNodeItems = new List<NodeItem>();
+
+                foreach (var selectedNode in this.SelectedNodes)
+                {
+                    NodeItem nodeItem = FindAssociatedNodeItem(selectedNode);
+                    if (nodeItem == null)
+                    {
+                        throw new ApplicationException("Unexpected code path!");
+                    }
+
+                    this.cachedSelectedNodeItems.Add(nodeItem);
+                }
+            }
+
+            // 
+            // Update the position of the node within the Canvas.
+            //
+            foreach (var nodeItem in this.cachedSelectedNodeItems)
+            {
+                nodeItem.X += e.HorizontalChange;
+                nodeItem.Y += e.VerticalChange;
+            }
+
+            var eventArgs = new NodeDraggingEventArgs(NodeDraggingEvent, this, this.SelectedNodes, e.HorizontalChange, e.VerticalChange);
+            RaiseEvent(eventArgs);
+        }
+
+        /// <summary>
+        /// Event raised when the user has finished dragging a node.
+        /// </summary>
+        private void NodeItem_DragCompleted(object source, NodeDragCompletedEventArgs e)
+        {
+            e.Handled = true;
+
+            var eventArgs = new NodeDragCompletedEventArgs(NodeDragCompletedEvent, this, this.SelectedNodes);
+            RaiseEvent(eventArgs);
+
+            if (cachedSelectedNodeItems != null)
+            {
+                cachedSelectedNodeItems = null;
+            }
+
+            this.IsDragging = false;
+            this.IsNotDragging = true;
+            this.IsDraggingNode = false;
+            this.IsNotDraggingNode = true;
+        }
+
+        #endregion Private Methods
+    }
+}
